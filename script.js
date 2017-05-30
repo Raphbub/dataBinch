6f026fcbefb69bb1c81bbfc72121e827a0e408f0
rxlacroix <romain.lacroix@unil.ch>
2017-05-31 00:46:40 +0200

    color scale

--- script.js
+++ script.js
@@ -77,11 +77,12 @@ let yScale = d3.scaleLinear()
                .range([height, 0])
                .nice();
 
-// Couleurs selon le SRM/EBC
+// Couleurs selon le ,SRM/EBC
 let SrmColorScale = d3.scaleLinear()
-                   .domain([1, 7, 10, 20, 35])
-                   // TODO affiner les couleurs
-                   .range(["#FFE699","gold","#FE9A2E","brown","#610B0B"]);
+                   .domain([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,50])
+                   .range(["#FFE699","#FFD878","#FFCA5A","#FFBF42","#FBB123","#F8A600","#F39C00","#EA8F00","#E58500","#DE7C00","#D77200","#CF6900","#CB6200",
+                          "#C35900","#BB5100","#B54C00","#B04500","#A63E00","#A13700","#9B3200","#952D00","#8E2900","#882300","#821E00","#7B1A00","#771900",
+                          "#701400","#6A0E00","#660D00","#5E0B00","#5A0A02","#600903","#520907","#4C0505","#470606","#440607","#3F0708","#3B0607","#3A070B", "#36080A","black"]);
 
 // Couleurs selon le bar
 let barColorScale = d3.scaleOrdinal(d3.schemeCategory20);
