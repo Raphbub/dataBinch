cb12015976eee47ae69c297e2f092e724af57c0d
Raphbub <raph.bub@gmail.com>
2017-06-01 14:41:27 +0200

    Pop-up supprimé, tooltip ajoutés

--- script.js
+++ script.js
@@ -109,8 +109,8 @@ let stamenLayer =  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrai
 
 // Définition de la carte
 let map = L.map('map', {
-  center: [46.52, 6.62],
-  zoom: 6
+  center: [46.52243400482112, 6.632995605468751],
+  zoom: 15
 });
 
 // Ajout des couches à la carte
@@ -413,7 +413,7 @@ var yAxis =   svgScat.append("g")
     let brasserie = binches.find(x => x.Brasserie === brass);
 
     let marker = new L.marker([brasserie.Lat, brasserie.Long], {icon: brassMarker})
-        .bindPopup(brasserie.Brasserie)
+        .bindTooltip(brasserie.Brasserie)
         .addTo(brassMarkers)
         .on("click", function (d){
           let selectedBrass = brasserie.Brasserie;
@@ -430,9 +430,6 @@ var yAxis =   svgScat.append("g")
             console.log("Brasserie choisie :" + selectedBrass);
 
         });
-      //  console.log(brasserie.Brasserie);
-
-
   });
 
 
@@ -453,16 +450,18 @@ var yAxis =   svgScat.append("g")
 
   // Loop through the data
   for (var i = 0; i < barsLsne.length; i++) {
-    var person = barsLsne[i];
+    var bar = barsLsne[i];
   //  console.log(person.Lat);
     // Create and save a reference to each marker
-    markers[person.Bar] = L.marker([person.Lat, person.Long], {
+    markers[bar.Bar] = L.marker([bar.Lat, bar.Long], {
+      icon: barMarker,
+      riseOnHover: true
     })
-    .bindPopup(person.Bar)
+    .bindTooltip(bar.Bar,  {className: 'barTooltip'})
     .addTo(map);
 
     // Add the ID
-    markers[person.Bar]._icon.id = person.Bar;
+    markers[bar.Bar]._icon.id = bar.Bar;
 
 
 //  console.log(markers);
